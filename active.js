b0c7956300fbd72b4ffa93f39d74860caf16ebb2
Createyouracccount <78135550+Createyouracccount@users.noreply.github.com>
2022-04-18 17:57:04 +0900

    first-project-version
    simple project
    another image -> soon upload

---
+++ active.js
@@ -0,0 +1,17 @@
+// slide를 autoplay 하도록 만들기 위한 code
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
